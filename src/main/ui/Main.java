0f8e92bfaf49f23da8ec4d8b038944f87e4f283d
gliu25 <gliu25@students.ubc.ca>
2024-03-06 19:10:43 -0800

    Revert "Merge remote-tracking branch 'origin/main'"
    This reverts commit 2703b83308df03cce6719b2744deb2d569f2f550, reversing
    changes made to 8f854a4feebef2e3415162ea3f175f0c4f66e00d.

---
+++ src/main/ui/Main.java
@@ -0,0 +1,11 @@
+package ui;
+
+import model.Event;
+import java.time.LocalTime;
+import model.Schedule;
+
+public class Main {
+    public static void main(String[] args) {
+        new ScheduleApp();
+    }
+}
